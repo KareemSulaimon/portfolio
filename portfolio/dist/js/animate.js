2912f3c8ed2c54a3bb19e298143ef9fa56681e88
kareem_sulaimon <106354820+KareemSulaimon@users.noreply.github.com>
2022-09-07 13:10:55 +0100

    Add files via upload

---
+++ portfolio/dist/js/animate.js
@@ -0,0 +1,9 @@
+
+
+    var typed = new Typed (".auto-type", {
+        strings: ["kareem Sulaimon", "Frontend Engineer", "Web3 Enthusiast","kareem Sulaimon",],
+        typeSpeed: 150,
+        backSpeed: 150,
+        Loop: true
+    })
+    
